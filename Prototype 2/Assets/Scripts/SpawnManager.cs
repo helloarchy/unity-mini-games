0bb8b7c70214c0d4a507ebd66023646f5228a08d
Robert Hardy <helloarchy@gmail.com>
2021-05-02 14:39:41 +0100

    Unit 2: * Spawn prefab animals in random positions * Add spawn manager to handle spawning

---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float _spawnRangeX = 20;
+    private float _spawnPositionZ = 20;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            var animalPrefab = animalPrefabs[Random.Range(0, animalPrefabs.Length)];
+            var spawnPosition = new Vector3(Random.Range(-_spawnRangeX, _spawnRangeX), 0, _spawnPositionZ);
+            Instantiate(
+                animalPrefab,
+                spawnPosition,
+                animalPrefab.transform.rotation
+            );
+        }
+    }
+}
