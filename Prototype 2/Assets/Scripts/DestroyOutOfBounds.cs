30989d1f95c68809cda4a8c9787d3125bc3fed97
Robert Hardy <helloarchy@gmail.com>
2021-05-02 14:18:54 +0100

    Unit 2: * Shoot pizza prefab from player * Destroy objects out of bound * Make animals prefabs

---
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float _topBound = 30;
+    private float _bottomBound = -10;
+    
+    void Update()
+    {
+        if (transform.position.z > _topBound || transform.position.z < _bottomBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
