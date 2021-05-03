04511bae77dcaa7a82a5a1d1c8dd5f73110e21df
Robert Hardy <helloarchy@gmail.com>
2021-05-03 14:09:16 +0100

    Unit 3.2: * Use box collider to get width for background repeating

--- Prototype 3/Assets/Scripts/RepeatBackground.cs
+++ Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
-    public float xOffset = 50.0f;
     private Vector3 _startPosition;
-    // Start is called before the first frame update
+    private float _repeatWidth;
+    
     void Start()
     {
         _startPosition = transform.position;
+        _repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < (_startPosition.x - xOffset))
+        if (transform.position.x < (_startPosition.x - _repeatWidth))
         {
             transform.position = _startPosition;
         }
