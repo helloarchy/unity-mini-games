a2c6450598c1ae800746bc8dbe41b8471a2bc7f6
Robert Hardy <helloarchy@gmail.com>
2021-05-03 12:47:50 +0100

    Unit 3.1: Player Force * Make obstacle prefab * Add spawn manager to handle obstacle spawning * Add move left script to obstacles to move them

---
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float moveSpeed = 30.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * (Time.deltaTime * moveSpeed));
+    }
+}
