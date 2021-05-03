f1638f9f005e8bd37316fa08374dfd0b9fad516c
Robert Hardy <helloarchy@gmail.com>
2021-05-03 11:26:57 +0100

    Unit 3.1: Jump force * Make player jump

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _playerRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+        _playerRigidbody.AddForce(Vector3.up * 1000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _playerRigidbody.AddForce(Vector3.up * 1000);
+        }
+    }
+}
