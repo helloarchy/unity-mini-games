2e2650a763c9e248f0ef70940fb0bca5ac721d7a
Robert Hardy <helloarchy@gmail.com>
2021-05-03 14:03:26 +0100

    Unit 3.2: * Rolling background isn't noticeable on tiling

---
+++ Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    public float xOffset = 50.0f;
+    private Vector3 _startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < (_startPosition.x - xOffset))
+        {
+            transform.position = _startPosition;
+        }
+    }
+}
