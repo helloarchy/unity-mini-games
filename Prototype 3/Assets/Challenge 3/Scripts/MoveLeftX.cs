aa1ef20d2039bf04d2c88a6b96364fcb7a0ee22f
Robert Hardy <helloarchy@gmail.com>
2021-07-17 21:22:53 +0100

    Complete challenge 3

--- Prototype 3/Assets/Challenge 3/Scripts/MoveLeftX.cs
+++ Prototype 3/Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class MoveLeftX : MonoBehaviour
 {
     public float speed;
-    private PlayerControllerX playerControllerScript;
+    private PlayerControllerX _playerControllerScript;
     private float leftBound = -10;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+        _playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // If game is not over, move to the left
-        if (playerControllerScript.gameOver)
+        if (_playerControllerScript.gameOver == false)
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.left * (speed * Time.deltaTime), Space.World);
         }
 
         // If object goes off screen that is NOT the background, destroy it
--- Prototype 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Prototype 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -1,51 +1,68 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public bool gameOver;
-
     public float floatForce;
-    private float gravityModifier = 1.5f;
-    private Rigidbody playerRb;
+
+    private Rigidbody _playerRb;
 
     public ParticleSystem explosionParticle;
     public ParticleSystem fireworksParticle;
 
-    private AudioSource playerAudio;
+    private AudioSource _playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
+    public AudioClip bounceSound;
 
+    private float gravityModifier = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Physics.gravity *= gravityModifier;
-        playerAudio = GetComponent<AudioSource>();
-
+        _playerAudio = GetComponent<AudioSource>();
+        _playerRb = GetComponent<Rigidbody>();
+        _playerAudio = GetComponent<AudioSource>();
+        
         // Apply a small upward force at the start of the game
-        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        _playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
 
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool isInBounds = CheckIfInBounds();
+        
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (Input.GetKey(KeyCode.Space) && !gameOver && isInBounds)
         {
-            playerRb.AddForce(Vector3.up * floatForce);
+            _playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
         }
     }
 
+
+    private bool CheckIfInBounds()
+    {
+        var positionY = transform.position.y;
+        return positionY < 12.5f && positionY > -1;
+    }
+    
+
     private void OnCollisionEnter(Collision other)
     {
+        Debug.Log($"Collided with {other.gameObject.tag}");
+        
+        
         // if player collides with bomb, explode and set gameOver to true
         if (other.gameObject.CompareTag("Bomb"))
         {
             explosionParticle.Play();
-            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            _playerAudio.PlayOneShot(explodeSound, 1.0f);
             gameOver = true;
             Debug.Log("Game Over!");
             Destroy(other.gameObject);
@@ -55,11 +72,18 @@ public class PlayerControllerX : MonoBehaviour
         else if (other.gameObject.CompareTag("Money"))
         {
             fireworksParticle.Play();
-            playerAudio.PlayOneShot(moneySound, 1.0f);
+            _playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
-
         }
 
+        // if player collides with ground, bounce
+        else if (other.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("So I should bounce now...");
+            
+            _playerRb.AddForce(Vector3.up * floatForce * 10, ForceMode.Impulse);
+            _playerAudio.PlayOneShot(bounceSound, 1.0f);
+        }
     }
 
 }
