0e3167b1a37f52c5f8a3a239be888c8ab8fb3525
Robert Hardy <helloarchy@gmail.com>
2021-07-18 20:54:22 +0100

    Destroy enemies on fall, spawn new one each time

--- Prototype 4/Assets/Scripts/EnemyController.cs
+++ Prototype 4/Assets/Scripts/EnemyController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float speed;
+    public float minY = -10;
+    
     private Rigidbody _enemyRb;
     private GameObject _player;
     
@@ -21,5 +23,11 @@ public class EnemyController : MonoBehaviour
     {
         var playerDirection = _player.transform.position - transform.position;
         _enemyRb.AddForce(playerDirection.normalized * speed);
+
+        if (transform.position.y < minY)
+        {
+            Debug.Log($"Destroying enemy at height {transform.position.y}");
+            Destroy(gameObject);
+        }
     }
 }
--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -6,17 +6,22 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public float spawnRange = 9;
+    public int enemyCount = 0;
     
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GetRandomPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<EnemyController>().Length;
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(1);
+        }
     }
 
     private Vector3 GetRandomPosition()
@@ -25,5 +30,14 @@ public class SpawnManager : MonoBehaviour
         var spawnY = Random.Range(-spawnRange, spawnRange);
 
          return new Vector3(spawnX, 0, spawnY);
+    }
+
+    private void SpawnEnemyWave(int numEnemies)
+    {
+        Debug.Log($"Spawning {numEnemies} enemies");
+        for (int i = 0; i < numEnemies; i++)
+        {
+            Instantiate(enemyPrefab, GetRandomPosition(), enemyPrefab.transform.rotation);
+        }
     }
 }
