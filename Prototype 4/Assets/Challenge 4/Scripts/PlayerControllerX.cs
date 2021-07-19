c26dd7333b070d9c688f9f119de7546feaa716ac
Robert Hardy <helloarchy@gmail.com>
2021-07-19 20:33:42 +0100

    Deflect enemy off player when power up

--- Prototype 4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Prototype 4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -25,7 +25,7 @@ public class PlayerControllerX : MonoBehaviour
     {
         // Add force to player in direction of the focal point (and camera)
         float verticalInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
+        playerRb.AddForce(focalPoint.transform.forward * (verticalInput * speed * Time.deltaTime)); 
 
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
@@ -57,14 +57,16 @@ public class PlayerControllerX : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer =  transform.position - other.gameObject.transform.position; 
+            Vector3 awayFromPlayer = other.gameObject.transform.position - transform.position; 
            
-            if (hasPowerup) // if have powerup hit enemy with powerup force
+            if (hasPowerup)
             {
+                // if have power up hit enemy with power up force
                 enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
             }
-            else // if no powerup, hit enemy with normal strength 
+            else
             {
+                // if no power up, hit enemy with normal strength 
                 enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
             }
 
