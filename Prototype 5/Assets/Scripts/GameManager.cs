4487a79d39973abbaf883f2b8c2057bd90ff96d2
Robert Hardy <helloarchy@gmail.com>
2021-07-20 21:40:13 +0100

    Spawn targets randomly

--- Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private float _spawnRate = 1.0f;
+    
     public List<GameObject> targets;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTargets());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator SpawnTargets()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_spawnRate);
+            var index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
     }
 }
