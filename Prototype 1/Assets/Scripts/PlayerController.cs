50b7b45ee7e0ba36ac07b08fc417f76715dd9580
Robert Hardy <helloarchy@gmail.com>
2021-07-23 21:14:30 +0100

    Use physics for acceleration

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 20.0f;
+    [SerializeField] private float horsePower = 0;
     private float _turnSpeed = 45.0f;
     private float _horizontalInput;
     private float _forwardInput;
+    private Rigidbody _playerRb;
+    [SerializeField] private GameObject centreOfMass;
+
+
+    private void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();
+        _playerRb.centerOfMass = centreOfMass.transform.position;
+    }
 
     void FixedUpdate()
     {
@@ -17,7 +27,8 @@ public class PlayerController : MonoBehaviour
         _forwardInput = Input.GetAxis("Vertical");
         
         // Move car forward
-        transform.Translate(Vector3.forward * (Time.deltaTime * speed * _forwardInput));
+        // transform.Translate(Vector3.forward * (Time.deltaTime * horsePower * _forwardInput));
+        _playerRb.AddRelativeForce(Vector3.forward * (Time.deltaTime * horsePower * _forwardInput));
         
         // Steer the car using input
         transform.Rotate(Vector3.up * (Time.deltaTime * _turnSpeed * _horizontalInput));
